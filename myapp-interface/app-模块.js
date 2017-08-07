9d4420e932cfe617e0852010b177804c5c6b2955
yexiaodong <2283138757@qq.com>
2017-08-07 11:49:14 +0800

    nodeJS学习

--- myapp-interface/app-模块.js
+++ myapp-interface/app-模块.js
@@ -1,31 +1,27 @@
 var express = require('express');
-var fs = require('fs');//文件系统
 var app = express();
 
 //托管静态文件
 app.use(express.static('public'));
 
+//中间件
+//没有挂载路径的中间件，应用的每个请求都会执行该中间件
+app.use(function (req,res,next) {
+    console.log('没有挂载路径的中间件，Time',Date.now());
+    next();
+});
+
+
+//监听3002
 var server = app.listen(3002, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
 
-//优化-404处理
+
+//优化-404处理 注意必须放最后
 app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
+    res.status(404).send('Sorry cant find that!');
 });
-
-//route：
-app.route('/book')
-    .get(function(req, res) {
-      res.send({data:'get a random book'});
-    })
-    .post(function(req, res) {
-      res.send({data:'add a book'});
-    })
-    .put(function(req, res) {
-      res.send({data:'update the book'});
-    });
-
