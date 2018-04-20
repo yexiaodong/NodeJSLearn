59f0c457ab5e45977428b1d88f38c763b600281a
yexiaodong <2283138757@qq.com>
2018-04-20 08:56:34 +0800

    链接SQL Server数据库

---
+++ myapp-interface/app-sqlserver.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user:'sa',
+        password:'pass@word2',
+        server:'42.51.32.132',
+        port:'19394',    
+        database:'zjyyj',
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('select * from Biz_SitePageConfig', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
