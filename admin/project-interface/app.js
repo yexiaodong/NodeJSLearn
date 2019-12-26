1c2b12072e34d7ef165042fbe4bebcf25da20c15
yexiaodong <2283138757@qq.com>
2019-12-26 09:14:11 +0800

    后台项目暂时停止，需要验证更多技术

--- admin/project-interface/app.js
+++ admin/project-interface/app.js
@@ -1,13 +1,22 @@
 var express = require('express')
+var expressJwt = require('express-jwt')
+
 //引入各个模块的接口
 const user = require('./router/user')
 
 var app = express()
-app.get('/', function (req, res){
+// app.use(expressJwt({
+//     secret: 'sert123' //签名的密钥 或 publickey
+// }).unless({
+//     path: ['/user/login']//指定路径不经过 Token 解析
+// }))
+
+
+app.get('/', function (req, res) {
     res.send({ data: 'Got a get request at /' })
 })
 
-app.use('/user',user)
+app.use('/user', user)
 
 
 var server = app.listen(3002, function () {
--- admin/project-interface/router/user.js
+++ admin/project-interface/router/user.js
@@ -1,7 +1,20 @@
 //用户类的接口
 var express = require('express')
+var bodyParser = require('body-parser');
+var jwt = require('express-jwt')
 var db = require('../tools/db.js')
 var router = express.Router()
+router.use(bodyParser.json());
+
+
+//登陆
+router.get('/login', function (req, res){
+    const token = 'Bearer ' + jwt.toString({
+        id:123,
+        admin: true
+    },'sert123',{expiresIn: 3600 * 24 * 3})
+    res.send({ data: token })
+})
 
 router.get('/', function (req, res){
     res.send({ data: '获取单个用户' })
@@ -10,6 +23,12 @@ router.get('/', function (req, res){
 router.get('/list', async (req, res)=>{
     let data = await db.query('select * from sys_user')
     res.send({ data: data })
+})
+
+router.post('/add', async (req, res)=>{
+    let data = await db.query('select * from sys_user')
+    console.log('名称：',req.param('name'),req.body)
+    res.send({ data: '添加' })
 })
 
 module.exports = router
